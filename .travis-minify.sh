cb1b08c906760ebb937c3081dd50e69982366913
Dominic <meiserdo@web.de>
2016-04-02 14:44:15 +0200

    empty file treatment for travis

--- .travis-minify.sh
+++ .travis-minify.sh
@@ -2,26 +2,32 @@
 
 git checkout master
 if [ "${CC: -3}" == "gcc" ]; then
-    for file in httpd/share/static/*.{js,css}; do
-	if [[ $file != *.min.* ]]; then
-	    target="${file%.*}.min.${file##*.}"
-	    echo "minifying $file to $target"
-	    yui-compressor --type ${file##*.} -o "$target" "$file"
-	    if [ "$(du "$target" | awk '{print $1}')" != 0 ]; then
-		git add "$target"
-	    fi
-	fi
-    done
-    for file in httpd/share/static/*.svg; do
-	if [[ $file != *.min.* ]]; then
-	    target="${file%.*}.min.${file##*.}"
-	    echo "minifying $file to $target"
-	    svgo -o "$target" -i "$file"
-	    if [ "$(du "$target" | awk '{print $1}')" != 0 ]; then
-		git add "$target"
-	    fi
-	fi
-    done
-    git commit -m "[skip ci] update minified css & js & svg"
-    git push
+	for file in httpd/share/static/*.{js,css}; do
+		if [[ $file != *.min.* ]]; then
+			target="${file%.*}.min.${file##*.}"
+			rm -f "$target" || true
+			echo "==> minifying $file to $target"
+			yui-compressor --type ${file##*.} -o "$target" "$file"
+			if [ "$(du "$target" | awk '{print $1}')" == 0 ]; then
+				echo " -> fail"
+				cp "$file" "$target"
+			fi
+			git add "$target"
+		fi
+	done
+	for file in httpd/share/static/*.svg; do
+		if [[ $file != *.min.* ]]; then
+			target="${file%.*}.min.${file##*.}"
+			rm -f "$target" || true
+			echo "==> minifying $file to $target"
+			svgo -o "$target" -i "$file"
+			if [ "$(du "$target" | awk '{print $1}')" == 0 ]; then
+				echo " -> fail"
+				cp "$file" "$target"
+			fi
+			git add "$target"
+		fi
+	done
+# 	git commit -m "[skip ci] update minified css & js & svg"
+# 	git push
 fi
